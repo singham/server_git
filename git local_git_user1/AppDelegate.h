ca08af61f9a788d827d3d677f86a371974d626b3
Atii <neo_ati@yahoo.com>
2013-02-12 13:50:18 +0200

    Initial Commit

---
+++ git local_git_user1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git local_git_user1
+//
+//  Created by Abdullah Atik on 2/12/13.
+//  Copyright (c) 2013 Abdullah Atik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git local_git_user1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git local_git_user1
+//
+//  Created by Abdullah Atik on 2/12/13.
+//  Copyright (c) 2013 Abdullah Atik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
