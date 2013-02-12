f0642cce21426dc3d7b946041a0120209a6e28d8
Atii <neo_ati@yahoo.com>
2013-02-12 14:56:15 +0200

    Third Commit from user 2

--- git local_git_user1/AppDelegate.m
+++ git local_git_user1/AppDelegate.m
@@ -38,6 +38,7 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"Third commit from user2"); 
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
